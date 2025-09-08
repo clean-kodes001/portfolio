de9a566be4598aa93a9af25c968fe123e4add41f
Miracle <anoibi47@gmail.com>
2025-09-08 17:27:42 +0100

    updates

--- src/config/portfolio.ts
+++ src/config/portfolio.ts
@@ -160,8 +160,7 @@ export const portfolioConfig = {
       tags: ["React Native", "PHP", "MYSQL","TypeScript", "HTML5", "CSS3"],
       image: "/coinly.jpg",
       link: {
-        live: "https://expo.dev/artifacts/eas/9X1tEhkMZVAbqDXX1d76s7.apk",
-        website: "https://thecoinlyapp.com",
+        website: "https://coinlyapp.io",
         text:'Download app'
       },
       featured: true 
@@ -174,7 +173,6 @@ export const portfolioConfig = {
       tags: ["React Native", "PHP", "MYSQL","TypeScript", "React"],
       image: "/swift.jpg",
       link: {
-        live: "https://expo.dev/artifacts/eas/eyAVxLSqMU2zXNLsoRJ8YC.apk",
         website: "https://swiftpickup.com.ng/",
         web_app: "https://app.swiftpickup.com.ng/",
       },
